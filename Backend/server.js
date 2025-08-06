846bc96868f127a0fd9da0f6c148fd46970e4a79
Shahzain Ahmed <shahzainahmed57@gmail.com>
2025-08-06 14:01:23 +0500

    init

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+// You can create your Express Server using these 3 Steps
+
+// Step 1 - Initialization
+const express = require('express'); // instance of express object
+const app = express(); // server has been created inside app
+
+// Step 2 - Define Routes
+
+// Default Route -> localhost:5001/
+app.get('/', function (req, res) {
+    res.send("This is the default page");
+});
+
+// Home Route -> localhost:5001/home
+app.get('/home', function (req, res) { // request (data → server), response (server → data)
+    res.send("This is the homepage");
+});
+
+// Notes Route -> localhost:5001/notes
+app.get('/notes', function (req, res) {
+    res.send("You are on Notes Route");
+});
+
+// Step 3 - Start Server on PORT
+// app.listen(5001); function is optional here.
+app.listen(5001, function () {
+    console.log("Server Started at PORT: 5001");
+});
